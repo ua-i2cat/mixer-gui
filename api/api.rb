58fbab511164d5ea9434a900ada3b5e84b8e87f1
Contreras, Ignacio <ignacio.contreras@i2cat.net>
2013-09-26 12:50:12 +0200

    Rename images. Grid list is generated with a for loop (inside liquid template). Sinatra app is now modular (some clean up needed). Add config.ru. The app can be run with 'bundle install' and then 'rackup'

--- api/api.rb
+++ api/api.rb
@@ -2,179 +2,256 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'liquid'
-require 'sinatra'
+require 'sinatra/base'
 require 'rmixer'
 
+class MixerAPI < Sinatra::Base
 
-m = RMixer::Mixer.new 'localhost', 7777
+  set :mixer, RMixer::Mixer.new('localhost', 7777)
+  set :grid, 0
 
-k2s =
-  lambda do |h|
-    Hash === h ?
-      Hash[
-        h.map do |k, v|
-          [k.respond_to?(:to_s) ? k.to_s : k, k2s[v]]
-        end
-      ] : h
+  def mixer_request
+    begin
+      yield
+    rescue RMixer::MixerError => e
+      status 500
+      { :error => e.message }.to_json
+    end
   end
 
-def mixer_request
-  begin
-    yield
-  rescue RMixer::MixerError => e
-    status 500
-    { :error => e.message }.to_json
+  def dashboard
+    
+    k2s =
+    lambda do |h|
+      Hash === h ?
+        Hash[
+          h.map do |k, v|
+            [k.respond_to?(:to_s) ? k.to_s : k, k2s[v]]
+          end
+        ] : h
+    end
+
+    streams = []
+    settings.mixer.streams.each do |s|
+      streams << k2s[s]
+    end
+    destinations = []
+    settings.mixer.destinations.each do |d|
+      destinations << k2s[d]
+    end
+
+    liquid :index, :locals => {
+      "streams" => streams,
+      "destinations" => destinations,
+      "grid" => settings.grid
+    }
   end
-end
 
-get '/' do
-  content_type :html
-  puts m.streams
-  puts k2s[m.streams]
-  puts m.destinations
-  puts k2s[m.destinations]
-
-  streams = []
-  m.streams.each do |s|
-    streams << k2s[s]
-  end
-  destinations = []
-  m.destinations.each do |d|
-    destinations << k2s[d]
+  get '/app' do
+    content_type :html
+    dashboard
   end
 
-  liquid :index, :locals => {
-    "streams" => streams,
-    "destinations" => destinations
-  }
-end
+  post '/app/streams/:id/disable' do
+    content_type :html
+    mixer_request do
+      settings.mixer.disable_stream(params[:id].to_i).to_json
+    end
+    redirect '/app'
+  end
 
+  post '/app/streams/:id/enable' do
+    content_type :html
+    mixer_request do
+      settings.mixer.enable_stream(params[:id].to_i).to_json
+    end
+    redirect '/app'
+  end
 
-post '/start' do
-  content_type :json
+  post '/app/streams/:id/remove' do
+    content_type :html
+    mixer_request do
+      settings.mixer.remove_stream(params[:id].to_i).to_json
+    end
+    redirect '/app'
+  end
 
-  if params.include?(:size)
-    width, height = options[:size].downcase.split('x')
+  post '/app/streams/add' do
+    content_type :html
+    if params[:size]
+      width, height = params[:size].downcase.split('x')
+    else
+      width = params[:width]
+      height = params[:height]
+    end      
     width = width.to_i
     height = height.to_i
+
+    options = {
+      :new_w => (params[:new_w] || width).to_i,
+      :new_h => (params[:new_h] || height).to_i,
+      :x => (params[:x] || 0).to_i,
+      :y => (params[:y] || 0).to_i,
+      :layer => (params[:layer] || 1).to_i
+    }
+    mixer_request do
+      settings.mixer.add_stream(width, height, options).to_json
+    end
+    redirect '/app'
   end
 
-  options = {
-    :width => width,
-    :height => height,
-    :max_streams => params[:max_streams].to_i,
-    :input_port => params[:input_port].to_i
+  post '/app/destinations/add' do
+    content_type :html
+    mixer_request do
+      settings.mixer.add_destination(params[:ip], params[:port].to_i).to_json
+    end
+    redirect '/app'
+  end
+
+  post '/app/destinations/:id/remove' do
+    content_type :html
+    mixer_request do
+      settings.mixer.remove_destination(params[:id].to_i).to_json
+    end
+    redirect '/app'
+  end
+
+  post '/app/grid' do
+    content_type :html
+    mixer_request do
+      # settings.mixer.set_grid(params[:id].to_i).to_json
+    end
+    settings.grid = params[:id].to_i
+    redirect '/app'
+  end
+
+
+  post '/start' do
+    content_type :json
+
+    if params.include?(:size)
+      width, height = options[:size].downcase.split('x')
+      width = width.to_i
+      height = height.to_i
+    end
+
+    options = {
+      :width => width,
+      :height => height,
+      :max_streams => params[:max_streams].to_i,
+      :input_port => params[:input_port].to_i
+      }
+
+    mixer_request do
+      settings.mixer.start(params).to_json
+    end
+  end
+
+  post '/streams/add' do
+    content_type :json
+    width, height = params[:size].downcase.split('x')
+    width = width.to_i
+    height = height.to_i
+    options = {
+      :new_w => (params[:new_w] || width).to_i,
+      :new_h => (params[:new_h] || height).to_i,
+      :x => (params[:x] || 0).to_i,
+      :y => (params[:y] || 0).to_i,
+      :layer => (params[:layer] || 1).to_i
+    }
+    mixer_request do
+      settings.mixer.add_stream(width, height, options).to_json
+    end
+  end
+
+  post '/streams/:id/remove' do
+    content_type :json
+    mixer_request do
+      settings.mixer.remove_stream(params[:id].to_i).to_json
+    end
+  end
+
+  get '/streams' do
+    content_type :json
+    mixer_request do
+      settings.mixer.streams.to_json
+    end
+  end
+
+  get '/streams/:id' do
+    content_type :json
+    mixer_request do
+      settings.mixer.stream(params[:id].to_i).to_json
+    end
+  end
+
+  post '/streams/:id/enable' do
+    content_type :json
+    mixer_request do
+      settings.mixer.enable_stream(params[:id].to_i).to_json
+    end
+  end
+
+  post '/streams/:id/disable' do
+    content_type :json
+    mixer_request do
+      settings.mixer.disable_stream(params[:id].to_i).to_json
+    end
+  end
+
+  post '/streams/:id/modify' do
+    content_type :json
+
+    id = params[:id].to_i
+    width = params[:width].to_i
+    height = params[:height].to_i
+
+    keep_text = params[:keep_aspect_ratio].downcase
+    
+    options = {
+      :x => params[:x].to_i,
+      :y => params[:y].to_i,
+      :layer => params[:layer].to_i,
+      :keep_aspect_ratio => (keep_text == "true" || keep_text == "1")
     }
 
-  mixer_request do
-    m.start(params).to_json
+    mixer_request do
+      settings.mixer.modify_stream(id, width, height, options).to_json
+    end
   end
-end
 
-post '/streams/add' do
-  content_type :json
-  width, height = params[:size].downcase.split('x')
-  width = width.to_i
-  height = height.to_i
-  options = {
-    :new_w => (params[:new_w] || width).to_i,
-    :new_h => (params[:new_h] || height).to_i,
-    :x => (params[:x] || 0).to_i,
-    :y => (params[:y] || 0).to_i,
-    :layer => (params[:layer] || 1).to_i
-  }
-  mixer_request do
-    m.add_stream(width, height, options).to_json
+  get '/destinations' do
+    content_type :json
+    mixer_request do
+      settings.mixer.destinations.to_json
+    end
   end
-end
 
-post '/streams/:id/remove' do
-  content_type :json
-  mixer_request do
-    m.remove_stream(params[:id].to_i).to_json
+  post '/destinations/add' do
+    content_type :json
+    mixer_request do
+      settings.mixer.add_destination(params[:ip], params[:port].to_i).to_json
+    end
   end
-end
 
-get '/streams' do
-  content_type :json
-  mixer_request do
-    m.streams.to_json
+  get '/destinations/:id' do
+    content_type :json
+    mixer_request do
+      settings.mixer.destination(params[:id].to_i).to_json
+    end
   end
-end
 
-get '/streams/:id' do
-  content_type :json
-  mixer_request do
-    m.stream(params[:id].to_i).to_json
+  post '/destinations/:id/remove' do
+    content_type :json
+    mixer_request do
+      settings.mixer.remove_destination(params[:id].to_i).to_json
+    end
   end
-end
 
-post '/streams/:id/enable' do
-  content_type :json
-  mixer_request do
-    m.enable_stream(params[:id].to_i).to_json
-  end
-end
-
-post '/streams/:id/disable' do
-  content_type :json
-  mixer_request do
-    m.disable_stream(params[:id].to_i).to_json
-  end
-end
-
-post '/streams/:id/modify' do
-  content_type :json
-
-  id = params[:id].to_i
-  width = params[:width].to_i
-  height = params[:height].to_i
-
-  keep_text = params[:keep_aspect_ratio].downcase
-  
-  options = {
-    :x => params[:x].to_i,
-    :y => params[:y].to_i,
-    :layer => params[:layer].to_i,
-    :keep_aspect_ratio => (keep_text == "true" || keep_text == "1")
-  }
-
-  mixer_request do
-    m.modify_stream(id, width, height, options).to_json
-  end
-end
-
-get '/destinations' do
-  content_type :json
-  mixer_request do
-    m.destinations.to_json
-  end
-end
-
-post '/destinations/add' do
-  content_type :json
-  mixer_request do
-    m.add_destination(params[:ip], params[:port].to_i).to_json
-  end
-end
-
-get '/destinations/:id' do
-  content_type :json
-  mixer_request do
-    m.destination(params[:id].to_i).to_json
-  end
-end
-
-post '/destinations/:id/remove' do
-  content_type :json
-  mixer_request do
-    m.remove_destination(params[:id].to_i).to_json
-  end
-end
-
-post '/stop' do
-  content_type :json
-  mixer_request do
-    m.stop.to_json
+  post '/stop' do
+    content_type :json
+    mixer_request do
+      settings.mixer.stop.to_json
+    end
   end
 end
