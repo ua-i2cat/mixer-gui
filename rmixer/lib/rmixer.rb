71aaddf99a3987ebaf103e735588b28aafefefe9
palau <palau@palau-i2cat.(none)>
2013-09-27 11:48:15 +0200

    Grid change 2x2 implemented. Needs testing

--- rmixer/lib/rmixer.rb
+++ rmixer/lib/rmixer.rb
@@ -1,5 +1,6 @@
 require "rmixer/version"
 require "connector"
+require "grids"
 
 
 module RMixer
@@ -27,6 +28,36 @@ module RMixer
 
     def destination(id)
       get_destination(id)
+    end
+
+    def set_grid(id)
+      layout_size = get_layout
+      grid = case id
+        when 0
+        when 1 #2x2
+          calc_regular_grid(2,2)
+          streams.zip(grid).each do |stream, grid|
+            if grid.nil?
+              disable_stream(stream[:id])
+            elsif stream.nil?
+            else
+              modify_stream(
+                stream[:id], 
+                (grid[:width]*layout_size[:width]).floor, 
+                (grid[:height]*layout_size[:height]).floor, 
+                :x => (grid[:x]*layout_size[:width]).floor,
+                :y => (grid[:y]*layout_size[:height]).floor
+              )
+            end
+          end
+        when 2 #3x2
+        when 3 #3x3
+        when 4 #upperleft
+        when 5 #downright
+        when 6 #lena
+        else
+          raise MixerError, "Invalid grid id"
+        end
     end
 
     def method_missing(name, *args, &block)
